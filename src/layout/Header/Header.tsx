1354f2a4d0343af96d57f92587b874db301f0c12
MickSDonovan <131672518+MickSDonovan@users.noreply.github.com>
2023-09-20 11:44:09 +0200

    :feat: Header Navbar

---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../Navbar/Navbar';
+import './Header copy.scss';
+
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>O&apos;Journey</h1>
+      <Navbar />
+    </header>
+  );
+}
+
+export default Header;
