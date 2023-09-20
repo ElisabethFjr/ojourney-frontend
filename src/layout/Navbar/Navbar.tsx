adb458774615b58ba5a797642c0cd76732f507ff
Elisabeth Faujour <efaujour@gmail.com>
2023-09-20 14:30:11 +0200

    :style: Navbar

--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -6,7 +6,29 @@ function Navbar() {
     <nav className="header-navbar">
       <ul className="header-navbar-list">
         <li>
-          <NavLink to="/signInSignUp">S&apos;inscrire/Se Connecter</NavLink>
+          <NavLink to="/signInSignUp" className="header-navbar-list-link">
+            S&apos;inscrire/Se Connecter
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/" className="header-navbar-list-link">
+            Mes voyages
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/" className="header-navbar-list-link">
+            Nouveau voyage
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/" className="header-navbar-list-link">
+            Profil
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/" className="header-navbar-list-link">
+            Se déconnecter
+          </NavLink>
         </li>
       </ul>
     </nav>
