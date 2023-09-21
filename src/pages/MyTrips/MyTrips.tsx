93987de0a81c7dd0595120e70e4d00115c481e65
MickSDonovan <131672518+MickSDonovan@users.noreply.github.com>
2023-09-21 11:17:40 +0200

    MyTrips

--- src/pages/MyTrips/MyTrips.tsx
+++ src/pages/MyTrips/MyTrips.tsx
@@ -1,70 +1,123 @@
-import { useState } from 'react';
 import './MyTrips.scss';
 import { Link } from 'react-router-dom';
 import plane from '../../assets/Images/plane.png';
+import pencil from '../../assets/Images/pencil.png';
+import bin from '../../assets/Images/bin.png';
 
 export default function MyTrips() {
-  const [darkMode, setDarkMode] = useState(false);
   return (
     <main>
-      {darkMode ? (
-        <div className="MyTrips">
-          <h1>Mes Voyages</h1>
-          <img className="plane" src={plane} alt="avion" />
-          <p className="text">Aucun voyage disponible.</p>
-          <p>Commencez en créant un nouveau voyage.</p>
+      <div className="MyTrip">
+        <div>
+          <h1 className="MyTrip-title">Mes Voyages</h1>
+        </div>
+        <img className="MyTrip-plane" src={plane} alt="avion" />
+        <p className="MyTrip-text">Aucun voyage disponible.</p>
+        <p>Commencez en créant un nouveau voyage.</p>
 
-          <Link to="/" className="create-trip">
-            + Créer un nouveau voyage{' '}
-          </Link>
-        </div>
-      ) : (
-        <div className="MyTrips2">
-          <h1>Mes Voyages</h1>
-          <article className="item">
-            <img className="plane2" src={plane} alt="" />
-            <div className="form">
-              <h1>Titre du voyage</h1>
-              <h3>Creé par XXX</h3>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
-                molestiae fugit in saepe laudantium accusantium veritatis quidem
-                cupiditate error, optio minus, magnam a temporibus consequuntur!
-                Nisi maiores quas soluta alias!
-              </p>
+        <Link to="/" className="MyTrip-button-trip">
+          + Créer un nouveau voyage
+        </Link>
+      </div>
+      <div className="MyTrips">
+        <h1 className="MyTrips-title">Mes Voyages</h1>
+        <article className="MyTrips-item">
+          <img className="MyTrips-item-plane" src={plane} alt="" />
+          <div className="MyTrips-item-form">
+            <div className="MyTrips-item-bloc">
+              <h1 className="MyTrips-item-bloc-title">Titre du voyage</h1>
+              <div className="Mytrips-item-bloc-logo">
+                <img
+                  src={pencil}
+                  alt="edit"
+                  className="MyTrips-item-bloc-logo-one"
+                />
+                <img
+                  src={bin}
+                  alt="bin"
+                  className="MyTrips-item-bloc-logo-two"
+                />
+              </div>
             </div>
-          </article>
-          <article className="item">
-            <img className="plane2" src={plane} alt="" />
-            <div className="form">
-              <h1>Titre du voyage</h1>
-              <h3>Creé par XXX</h3>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
-                molestiae fugit in saepe laudantium accusantium veritatis quidem
-                cupiditate error, optio minus, magnam a temporibus consequuntur!
-                Nisi maiores quas soluta alias!
-              </p>
+            <h3>
+              Creé par
+              <Link to="/" target="_blank" rel="">
+                XXX
+              </Link>
+            </h3>
+            <p className="MyTrips-item-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+              molestiae fugit in saepe laudantium accusantium veritatis quidem
+              cupiditate error, optio minus, magnam a temporibus consequuntur!
+              Nisi maiores quas soluta alias!
+            </p>
+          </div>
+        </article>
+        <article className="MyTrips-item">
+          <img className="MyTrips-item-plane" src={plane} alt="" />
+          <div className="MyTrips-item-form">
+            <div className="MyTrips-item-bloc">
+              <h1 className="MyTrips-item-bloc-title">Titre du voyage</h1>
+              <div className="Mytrips-item-bloc-logo">
+                <img
+                  src={pencil}
+                  alt="edit"
+                  className="MyTrips-item-bloc-logo-one"
+                />
+                <img
+                  src={bin}
+                  alt="bin"
+                  className="MyTrips-item-bloc-logo-two"
+                />
+              </div>
             </div>
-          </article>
-          <article className="item">
-            <img className="plane2" src={plane} alt="" />
-            <div className="form">
-              <h1>Titre du voyage</h1>
-              <h3>Creé par XXX</h3>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
-                molestiae fugit in saepe laudantium accusantium veritatis quidem
-                cupiditate error, optio minus, magnam a temporibus consequuntur!
-                Nisi maiores quas soluta alias!
-              </p>
+            <h3>
+              Creé par
+              <Link to="/" target="_blank" rel="">
+                XXX
+              </Link>
+            </h3>
+            <p className="MyTrips-item-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+              molestiae fugit in saepe laudantium accusantium veritatis quidem
+              cupiditate error, optio minus, magnam a temporibus consequuntur!
+              Nisi maiores quas soluta alias!
+            </p>
+          </div>
+        </article>
+        <article className="MyTrips-item">
+          <img className="MyTrips-item-plane" src={plane} alt="" />
+          <div className="MyTrips-item-form">
+            <div className="MyTrips-item-bloc">
+              <h1 className="MyTrips-item-bloc-title">Titre du voyage</h1>
+              <div className="Mytrips-item-bloc-logo">
+                <img
+                  src={pencil}
+                  alt="edit"
+                  className="MyTrips-item-bloc-logo-one"
+                />
+                <img
+                  src={bin}
+                  alt="bin"
+                  className="MyTrips-item-bloc-logo-two"
+                />
+              </div>
             </div>
-          </article>
-        </div>
-      )}
-      <button type="button" onClick={() => setDarkMode(!darkMode)}>
-        Activer le {darkMode ? 'MyTrips2' : 'MyTrips'}
-      </button>
+            <h3>
+              Creé par
+              <Link to="/" target="_blank" rel="">
+                XXX
+              </Link>
+            </h3>
+            <p className="MyTrips-item-description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+              molestiae fugit in saepe laudantium accusantium veritatis quidem
+              cupiditate error, optio minus, magnam a temporibus consequuntur!
+              Nisi maiores quas soluta alias!
+            </p>
+          </div>
+        </article>
+      </div>
     </main>
   );
 }
