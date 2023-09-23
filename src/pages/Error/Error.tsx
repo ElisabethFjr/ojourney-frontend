5959e58373458ffdab3d816b66ac944a84094a87
Elisabeth Faujour <efaujour@gmail.com>
2023-09-23 16:39:19 +0200

    :feat: page Error + Route *

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,11 @@
+import Main from '../../layout/Main/Main';
+
+import './Error.scss';
+
+function Error() {
+  return (
+    <Main>Whoops ! Il semblerait que cette page soit partie en vacances.</Main>
+  );
+}
+
+export default Error;
