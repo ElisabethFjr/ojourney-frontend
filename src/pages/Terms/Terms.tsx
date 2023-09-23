d4402626059a599e4d882e49faf99bef0734d5ab
Elisabeth Faujour <efaujour@gmail.com>
2023-09-23 13:19:10 +0200

    :feat: create base pages About/Contact/Terms

---
+++ src/pages/Terms/Terms.tsx
@@ -0,0 +1,13 @@
+import Main from '../../layout/Main/Main';
+
+import './Terms.scss';
+
+function Terms() {
+  return (
+    <Main>
+      <h1 className="main-title">Mentions Légales</h1>
+    </Main>
+  );
+}
+
+export default Terms;
