8a0029aaa4a5c45d4ef1805c04f39315027f97bd
Elisabeth Faujour <efaujour@gmail.com>
2023-09-25 16:03:54 +0200

    :feat: fix FlashMessage animation

--- src/components/FlashMessage/FlashMessage.tsx
+++ src/components/FlashMessage/FlashMessage.tsx
@@ -1,38 +1,18 @@
-import { useState, useEffect } from 'react';
 import './FlashMessage.scss';
 
 export interface Flash {
   type: 'success' | 'error';
-  text: string;
+  children: React.ReactNode;
   duration?: number; // optionnel
 }
 
-function FlashMessage({ type, duration = 3000, text }: Flash) {
-  const [isVisible, setIsVisible] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsVisible(false);
-    }, duration);
-
-    // Cleanup on component unmount
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [duration]);
-
-  if (!isVisible) {
-    return null; // Si le message n'est pas visible, ne renvoyez rien.
-  }
-
-  console.log('Flash message render');
-
+function FlashMessage({ type, duration, children }: Flash) {
   return (
     <div
       className={`flash flash--${type}`}
-      style={{ animationDuration: `${duration}ms` }}
+      style={{ animationDuration: `${duration ?? 3000}ms` }}
     >
-      <p className="flash-message">{text}</p>
+      {children}
     </div>
   );
 }
