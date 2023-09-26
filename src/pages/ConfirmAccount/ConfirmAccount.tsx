b1612ba56049af3daacda10aef2d1f59c428dca2
MickSDonovan <131672518+MickSDonovan@users.noreply.github.com>
2023-09-26 11:59:03 +0200

    :feat: confirmEmail get API token

--- src/pages/ConfirmAccount/ConfirmAccount.tsx
+++ src/pages/ConfirmAccount/ConfirmAccount.tsx
@@ -1,20 +1,58 @@
+import { useState, useEffect } from 'react';
+import axiosInstance from '../../utils/axios';
+
 import ButtonColor from '../../components/Button/ButtonColor/ButtonColor';
 import Main from '../../layout/Main/Main';
 
 import './ConfirmAccount.scss';
 
 function ConfirmAccount() {
+  const [isConfirmed, setIsConfirmed] = useState(false);
+  const token = new URLSearchParams(document.location.search);
+
+  useEffect(() => {
+    async function confirmEmail() {
+      await axiosInstance
+        .get(`/confirm?confirm=${token}`)
+        .then((response) => {
+          setIsConfirmed(true);
+          console.log('Réponse ok');
+          console.log(`Réponse: ${response.data}`);
+        })
+        .catch((error) => {
+          setIsConfirmed(false);
+          console.log('Erreur');
+          console.log(error);
+        });
+    }
+    confirmEmail();
+  });
+
   return (
     <Main>
       <section className="confirm-container">
-        <h1 className="main-title confirm-title">
-          Bravo ! Votre compte a été créé avec succès.
-        </h1>
-        <p className="confirm-text">
-          Vous pouvez maintenant vous connecter en cliquant sur le bouton
-          ci-dessous.
-        </p>
-        <ButtonColor text="Se Connecter" to="/signin-signup" />
+        {isConfirmed ? (
+          <>
+            <h1 className="main-title confirm-title">
+              Bravo ! Votre compte a été créé avec succès.
+            </h1>
+            <p className="confirm-text">
+              Vous pouvez maintenant vous connecter en cliquant sur le bouton
+              ci-dessous.
+            </p>
+            <ButtonColor text="Se Connecter" to="/signin-signup" />
+          </>
+        ) : (
+          <>
+            <h1 className="main-title confirm-title">
+              Désolé, la confirmation de compte a échoué.
+            </h1>
+            <p className="confirm-text">
+              {' '}
+              Veuillez réessayer plus tard ou nous contacter.
+            </p>
+          </>
+        )}
       </section>
     </Main>
   );
