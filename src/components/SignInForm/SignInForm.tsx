19634be84fd05038cbf581acee7b7b907a776bab
Meganedeamorin <megane.de-amorin@oclock.school>
2023-09-27 10:18:57 +0200

    :feat:signInForm handle Submit + dispatch login thunk

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -1,12 +1,38 @@
+import { FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { login } from '../../store/reducers/user';
+
 import InputField from '../InputField/InputField';
 import ButtonSubmit from '../Button/ButtonSubmit/ButtonSubmit';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 
 import './SignInForm.scss';
 
 function SignInForm() {
+  // Initialize Hooks
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  
+  // Get states from Redux 
+  const pseudo = useAppSelector((state) => state.user.pseudo);
+  const errorMessage = useAppSelector((state) => state.user.errorMessage);
+
+  // Handle SignIn form submit
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    dispatch(login(formData));
+    // navigate('/my-trips');
+  };
+
   return (
     <div className="form-content">
-      <form className="form-element">
+      <form className="form-element" onSubmit={handleSubmit}>
+        {errorMessage && (
+          <ErrorMessage icon="fa-solid fa-xmark" text={errorMessage} />
+        )}
         <InputField
           name="email"
           placeholder="Email"
