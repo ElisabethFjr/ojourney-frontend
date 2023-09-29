c11d6082a6dc7a297a166d5363b33c7e9b8d6d07
Elisabeth Faujour <efaujour@gmail.com>
2023-09-29 09:00:39 +0200

    :fix: NewPassword page onClick, form, onSubmit

--- src/pages/NewPassword/NewPassword.tsx
+++ src/pages/NewPassword/NewPassword.tsx
@@ -3,49 +3,48 @@ import Button from '../../components/Button/Button';
 
 import './NewPassword.scss';
 
-function NewPassword () {
+function NewPassword() {
+  const handleClickCancel = () => {
+    console.log('Au clic sur le bouton, revenir en arrière');
+  };
 
+  const handleClickSubmit = () => {
+    console.log('Au clic sur le bouton, valider le modification de mdp');
+  };
   return (
-    <div className="new-password-main"> 
+    <div className="new-password-main">
+      <h1 className="new-password-title">Nouveau mot de passe</h1>
 
-    <h1 className="new-password-title">Nouveau mot de passe</h1>
+      <form
+        className="new-password-input-container"
+        onSubmit={handleClickSubmit}
+      >
+        <InputField
+          name="password"
+          placeholder="Mot de passe"
+          type="password"
+          icon="fa-solid fa-lock"
+        />
 
-    <div className="new-password-input-container">
-    <InputField
-    name="password"
-    placeholder="Mot de passe"
-    type="password"
-    icon="fa-solid fa-lock"
-  />
+        <InputField
+          name="confirmation"
+          placeholder="Mot de passe (confirmation)"
+          type="password"
+          icon="fa-solid fa-lock"
+        />
+      </form>
 
-  <InputField
-  name="confirmation"
-  placeholder="Mot de passe (confirmation)"
-  type="password"
-  icon="fa-solid fa-lock"
-/>
-
-</div>
-
-<div className="new-password-btn-container">
-<Button
-      text="Annuler"
-      type="button"
-      customClass="outline-dark"
-      onClick="outline"
-    />
-<Button
-      text="Confirmer"
-      type="button"
-      customClass="outline-dark"
-      onClick="outline"
-    />
+      <div className="new-password-btn-container">
+        <Button
+          text="Annuler"
+          type="button"
+          customClass="outline-dark"
+          onClick={handleClickCancel}
+        />
+        <Button text="Confirmer" type="submit" customClass="outline-dark" />
+      </div>
     </div>
-
-    </div>
-    
-  )
-
+  );
 }
 
 export default NewPassword;
