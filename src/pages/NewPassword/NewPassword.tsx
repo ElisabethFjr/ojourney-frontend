e46637ad917d77026388295bfce74c63716b1ecf
Elisabeth Faujour <efaujour@gmail.com>
2023-09-29 12:30:27 +0200

    :style: style NewPassword MEGANE

--- src/pages/NewPassword/NewPassword.tsx
+++ src/pages/NewPassword/NewPassword.tsx
@@ -1,3 +1,5 @@
+import Main from '../../layout/Main/Main';
+
 import InputField from '../../components/InputField/InputField';
 import Button from '../../components/Button/Button';
 
@@ -12,38 +14,35 @@ function NewPassword() {
     console.log('Au clic sur le bouton, valider le modification de mdp');
   };
   return (
-    <div className="new-password-main">
-      <h1 className="new-password-title">Nouveau mot de passe</h1>
+    <Main>
+      <h1 className="main-title new-password-title">Nouveau mot de passe</h1>
+      <div className="new-password-container">
+        <form className="new-password-form" onSubmit={handleClickSubmit}>
+          <InputField
+            name="password"
+            placeholder="Nouveau Mot de passe"
+            type="password"
+            icon="fa-solid fa-lock"
+          />
 
-      <form
-        className="new-password-input-container"
-        onSubmit={handleClickSubmit}
-      >
-        <InputField
-          name="password"
-          placeholder="Mot de passe"
-          type="password"
-          icon="fa-solid fa-lock"
-        />
-
-        <InputField
-          name="confirmation"
-          placeholder="Mot de passe (confirmation)"
-          type="password"
-          icon="fa-solid fa-lock"
-        />
-      </form>
-
-      <div className="new-password-btn-container">
-        <Button
-          text="Annuler"
-          type="button"
-          customClass="outline-dark"
-          onClick={handleClickCancel}
-        />
-        <Button text="Confirmer" type="submit" customClass="outline-dark" />
+          <InputField
+            name="confirmation"
+            placeholder="Mot de passe (confirmation)"
+            type="password"
+            icon="fa-solid fa-lock"
+          />
+          <div className="new-password-btn-container">
+            <Button
+              text="Annuler"
+              type="button"
+              customClass="outline-dark"
+              onClick={handleClickCancel}
+            />
+            <Button text="Confirmer" type="submit" customClass="color" />
+          </div>
+        </form>
       </div>
-    </div>
+    </Main>
   );
 }
 
