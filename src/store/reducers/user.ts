bf5e709c3e0abf588bbde429bb7a74546fbbf7a3
MickSDonovan <131672518+MickSDonovan@users.noreply.github.com>
2023-10-03 14:53:56 +0200

    :feat: action updateUserDara

--- src/store/reducers/user.ts
+++ src/store/reducers/user.ts
@@ -47,6 +47,13 @@ export const initialState: UserState = {
   env: null,
 };
 
+// Create action update user data
+export const updateUserData = createAction<{
+  firstname?: string | null;
+  lastname?: string | null;
+  email?: string | null;
+}>('user/updateUserData');
+
 // Create Logout action
 export const logout = createAction('user/logout');
 
@@ -94,6 +101,17 @@ export const login = createAsyncThunk(
 // Create User Reducer
 const userReducer = createReducer(initialState, (builder) => {
   builder
+    // Update user data => addCase()
+    .addCase(updateUserData, (state, action) => {
+      const { firstname, lastname, email } = action.payload;
+
+      state.data = {
+        ...state.data,
+        firstname: firstname !== undefined ? firstname : state.data.firstname,
+        lastname: lastname !== undefined ? lastname : state.data.lastname,
+        email: email !== undefined ? email : state.data.email,
+      };
+    })
     // Login promise pending
     .addCase(login.pending, (state) => {
       state.errorMessage = null;
