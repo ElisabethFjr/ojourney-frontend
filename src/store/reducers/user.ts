6b380b70375ffba2c8751613aa40623dd172e1eb
Elisabeth Faujour <efaujour@gmail.com>
2023-10-03 10:44:58 +0200

    :fix: Change env user.ts

--- src/store/reducers/user.ts
+++ src/store/reducers/user.ts
@@ -44,7 +44,7 @@ export const initialState: UserState = {
   },
   isConnected: false,
   errorMessage: null,
-  env: 'dev',
+  env: null,
 };
 
 // Create Logout action
@@ -54,7 +54,7 @@ export const logout = createAction('user/logout');
 export const login = createAsyncThunk(
   'user/login',
   async (formData: FormData) => {
-    const env = 'dev';
+    const env = null;
     let axiosOptions = {};
     if (env === 'dev') {
       axiosOptions = {};
