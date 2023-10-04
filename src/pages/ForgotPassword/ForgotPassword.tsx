95147e394da891cc2b81fe388eb4805c6fc8ae1f
Meganedeamorin <megane.de-amorin@oclock.school>
2023-10-04 19:36:39 +0200

    :feat:ForgotPasswordHandleSubmit

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState  }from 'react';
+import axiosInstance from '../../utils/axios';
 
 import InputField from '../../components/InputField/InputField';
 import Button from '../../components/Button/Button';
@@ -7,14 +8,27 @@ import './ForgotPassword.scss';
 
 function ForgotPassword () {
 
- // const 
+  const [email, setEmail] = useState('');
 
- // const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-   // event.preventDefault();
+ const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+   event.preventDefault();
 
- // }
+   if (!email) {
 
- //onSubmit={handleSubmit}>
+    return;
+
+  }
+
+  try {
+
+    const response = await axiosInstance.post('/new-password', { email });
+
+  }
+ catch (error) {
+  console.error("Une erreur est survenue lors de la rénilitilisation du mot de passe", error);
+}
+};
+
   return (
     <section className="forgot-password-container">
 
@@ -22,12 +36,13 @@ function ForgotPassword () {
   
 <h2 className='forgot-password-subtitle'>Veuillez saisir l'adresse mail associée à votre compte.</h2>
 
-  <form className='forgot-password-form'>
+  <form className='forgot-password-form'  onSubmit={handleSubmit}>
   <InputField
               name="email"
               placeholder="Votre e-mail"
               type="email"
               icon="fa-solid fa-at"
+              required
             />
           <Button
             text="Confirmer"
