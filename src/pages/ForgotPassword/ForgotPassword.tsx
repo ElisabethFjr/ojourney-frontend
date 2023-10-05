7b33c6db80aee27dc34f9a321e4ee4a55f49725e
Meganedeamorin <megane.de-amorin@oclock.school>
2023-10-05 10:36:05 +0200

    :feat:ForgotPasswordPage

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -4,18 +4,13 @@ import axiosInstance from '../../utils/axios';
 
 import InputField from '../../components/InputField/InputField';
 import Button from '../../components/Button/Button';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
 import ModalForgotPassword from '../../components/ModalForgotPassword/ModalForgotPassword';
 
 import './ForgotPassword.scss';
 
 function ForgotPassword () {
 
-  const [email, setEmail] = useState('');
-
- const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-   event.preventDefault();
-
-  }
 
   return (
     <section className="forgot-password-container">
@@ -23,8 +18,11 @@ function ForgotPassword () {
   <h1 className="forgot-password-title">Réinitialiser le mot de passe</h1>
   
 <h2 className='forgot-password-subtitle'>Veuillez saisir l'adresse mail associée à votre compte.</h2>
-<ModalForgotPassword></ModalForgotPassword>
   <form className='forgot-password-form'  onSubmit={handleSubmit}>
+  {showModalForgotPassword&& <ModalForgotPassword />}
+      {errorMessage && (
+        <ErrorMessage icon="fa-solid fa-xmark" text={errorMessage} />
+      )}
   <InputField
               name="email"
               placeholder="Votre e-mail"
@@ -36,6 +34,8 @@ function ForgotPassword () {
             text="Confirmer"
             customClass="color button-style--width"
             type="submit"
+
+           
           />
           
   </form>
