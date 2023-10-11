b300a2f0e3064ee152cc0d89add2ff22e9eef19e
Elisabeth Faujour <efaujour@gmail.com>
2023-10-11 16:16:10 +0200

    :fix: Env null

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -16,7 +16,7 @@ if (localStorage.getItem('userToken')) {
 }
 
 // Check environment and set credentials if not in development
-const env = 'dev';
+const env = null;
 if (env !== 'dev') {
   axiosInstance.defaults.withCredentials = true;
 }
